71bb610d27845dff8192df79ad606807f04023d6
Damir Bolic <bolicd@gmail.com>
2022-03-23 12:50:27 +0100

    Initial implementation

---
+++ DapperExtensions/AsyncDapperExtensions/DapperExtensionsAsync.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using Dapper;
+
+namespace AsyncDapperExtensions;
+
+public static class DapperExtensionsAsync
+{
+    public static async Task<int> ExecuteAsyncWithToken(this IDbConnection cnn,
+        string sql, 
+        object param = null!,
+        IDbTransaction transaction = null!,
+        int? commandTimeout = null, 
+        CommandType? commandType = null,
+        CancellationToken cancellationToken = default)
+    {
+        return await cnn.ExecuteAsync(
+            new CommandDefinition(sql, param, transaction, commandTimeout, commandType, cancellationToken: cancellationToken));
+    }   
+
+    public static async Task<IEnumerable<T>> QueryAsyncWithToken<T>(this IDbConnection cnn,
+        string sql,
+        object? param = null,
+        IDbTransaction? transaction = null,
+        int? commandTimeout = null,
+        CommandType? commandType = null,
+        CancellationToken cancellationToken = default)
+    {
+        return await cnn.QueryAsync<T>(new CommandDefinition(sql, param, transaction, commandTimeout, commandType,
+            cancellationToken: cancellationToken));
+    }
+}
---
+++ DapperExtensions/Tests/DbTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Tests.Infrastructure;
+
+namespace Tests;
+
+[TestFixture]
+public class DbTests : IntegrationTestBase
+{
+    [Test]
+    public void TestThis()
+    {
+        Assert.AreEqual(1,1);
+    }
+    
+}
